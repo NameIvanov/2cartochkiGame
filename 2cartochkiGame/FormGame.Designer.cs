ebf8541e09061328212059bd367c094edd0334cb
NameIvanov <toster.one@yandex.ru>
2025-05-25 20:19:42 +0300

    Добавьте файлы проекта.

---
+++ 2cartochkiGame/FormGame.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace _2cartochkiGame
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.buttonLevel1 = new System.Windows.Forms.Button();
+            this.buttonLevel2 = new System.Windows.Forms.Button();
+            this.textBoxCountSteps = new System.Windows.Forms.TextBox();
+            this.textBoxPerfect = new System.Windows.Forms.TextBox();
+            this.timerSleep = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 120;
+            this.dataGridView1.Size = new System.Drawing.Size(485, 483);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Column1";
+            this.Column1.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Column2";
+            this.Column2.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "Column3";
+            this.Column3.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "Column4";
+            this.Column4.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.BackColor = System.Drawing.Color.Tomato;
+            this.buttonRestart.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRestart.ForeColor = System.Drawing.Color.Black;
+            this.buttonRestart.Location = new System.Drawing.Point(572, 536);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(139, 31);
+            this.buttonRestart.TabIndex = 2;
+            this.buttonRestart.Text = "Начать заново";
+            this.buttonRestart.UseVisualStyleBackColor = false;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // buttonLevel1
+            // 
+            this.buttonLevel1.BackColor = System.Drawing.Color.Tomato;
+            this.buttonLevel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLevel1.Location = new System.Drawing.Point(560, 52);
+            this.buttonLevel1.Name = "buttonLevel1";
+            this.buttonLevel1.Size = new System.Drawing.Size(170, 50);
+            this.buttonLevel1.TabIndex = 3;
+            this.buttonLevel1.Text = "Уровень 1";
+            this.buttonLevel1.UseVisualStyleBackColor = false;
+            this.buttonLevel1.Click += new System.EventHandler(this.buttonLevel1_Click);
+            // 
+            // buttonLevel2
+            // 
+            this.buttonLevel2.BackColor = System.Drawing.Color.Tomato;
+            this.buttonLevel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLevel2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonLevel2.Location = new System.Drawing.Point(560, 127);
+            this.buttonLevel2.Name = "buttonLevel2";
+            this.buttonLevel2.Size = new System.Drawing.Size(170, 50);
+            this.buttonLevel2.TabIndex = 4;
+            this.buttonLevel2.Text = "Уровень 2";
+            this.buttonLevel2.UseVisualStyleBackColor = false;
+            this.buttonLevel2.Click += new System.EventHandler(this.buttonLevel2_Click);
+            // 
+            // textBoxCountSteps
+            // 
+            this.textBoxCountSteps.BackColor = System.Drawing.Color.NavajoWhite;
+            this.textBoxCountSteps.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxCountSteps.Location = new System.Drawing.Point(12, 501);
+            this.textBoxCountSteps.Name = "textBoxCountSteps";
+            this.textBoxCountSteps.ReadOnly = true;
+            this.textBoxCountSteps.Size = new System.Drawing.Size(334, 32);
+            this.textBoxCountSteps.TabIndex = 1;
+            this.textBoxCountSteps.Text = "Кол-во сделанных шагов: 0";
+            // 
+            // textBoxPerfect
+            // 
+            this.textBoxPerfect.BackColor = System.Drawing.Color.NavajoWhite;
+            this.textBoxPerfect.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxPerfect.Location = new System.Drawing.Point(12, 539);
+            this.textBoxPerfect.Name = "textBoxPerfect";
+            this.textBoxPerfect.ReadOnly = true;
+            this.textBoxPerfect.Size = new System.Drawing.Size(334, 32);
+            this.textBoxPerfect.TabIndex = 5;
+            this.textBoxPerfect.Text = "Кол-во допустимых шагов: 30";
+            // 
+            // timerSleep
+            // 
+            this.timerSleep.Interval = 700;
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(786, 581);
+            this.Controls.Add(this.textBoxPerfect);
+            this.Controls.Add(this.buttonLevel2);
+            this.Controls.Add(this.buttonLevel1);
+            this.Controls.Add(this.buttonRestart);
+            this.Controls.Add(this.textBoxCountSteps);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormGame";
+            this.Text = "Игра Парные Карточки";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Cyan;
+            this.Load += new System.EventHandler(this.FormGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonRestart;
+        private System.Windows.Forms.Button buttonLevel1;
+        private System.Windows.Forms.Button buttonLevel2;
+        private System.Windows.Forms.TextBox textBoxCountSteps;
+        private System.Windows.Forms.TextBox textBoxPerfect;
+        private System.Windows.Forms.Timer timerSleep;
+        private System.Windows.Forms.DataGridViewImageColumn Column1;
+        private System.Windows.Forms.DataGridViewImageColumn Column2;
+        private System.Windows.Forms.DataGridViewImageColumn Column3;
+        private System.Windows.Forms.DataGridViewImageColumn Column4;
+    }
+}
+
---
+++ 2cartochkiGame/FormGame.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace _2cartochkiGame
+{
+    public partial class FormGame : Form
+    {
+        private void ShowItem(int[,] znach, int row, int col) //ОТРИСОВКА НА ЭКРАНЕ ВЫБРАННОЙ КАРТОЧКИ
+        {
+            int num = znach[row, col];
+            if(num<0)
+            {
+                string ph = $"images/photo_{Convert.ToString((num)).Substring(1)}.png";
+                Image image = Image.FromFile(ph);
+                dataGridView1.Rows[row].Cells[col].Value = image;
+            }
+            else
+            {
+                string ph = $"images/photo{Convert.ToString((num))}.jpg";
+                Image image = Image.FromFile(ph);
+                dataGridView1.Rows[row].Cells[col].Value = image;
+            }
+        }
+        private void ShowDefolt() //ВЫВОД НАЧАЛЬНОГО ЭКРАНА КАРТОЧЕК
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = Def;
+                }
+            }
+        }
+        private void completionMass(int[,] mass,List<int> words1)// ПЕРЕМЕШИВАНИЕ "ФОТОГРАФИЙ" ПО МАССИВУ
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    int w = rnd.Next(0, words1.Count);
+                    mass[i, j] = words1[w];
+                    words1.RemoveAt(w);
+                }
+            }
+        }
+        public void resetFull()// СБРОС ВСЕГО ИГРОВОГО ПОЛЯ
+        {
+            cntSteps = 0;
+            cnt = 0;
+            cntEnd = 30;
+            firstIndex1 = -1;
+            secondIndex1 = -1;
+            textBoxCountSteps.Text = $"Кол-во сделанных шагов:{cntSteps}";
+            textBoxPerfect.Text = $"Кол-во допустимых шагов: {cntEnd}";
+            ShowDefolt();
+            if (flag == 0)
+            {
+                List<int> level1_words = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, -1, -2, -3, -4, -5, -6, -7, -8 };
+                completionMass(mass, level1_words);
+            }
+            else
+            {
+                List<int> level2_words = new List<int> { 9, 10, 11, 12, 13, 14, 15, 16, -9, -10, -11, -12, -13, -14, -15, -16 };
+                completionMass(mass, level2_words);
+            }
+        }
+        int[,] mass = new int[4, 4]; // Массив для значенией, чтоб потом по значению присвоить карточку в датагрид
+        Random rnd = new Random();
+        Image Def;
+        int firstIndex1 = -1;
+        int secondIndex1 = -1;
+        int firstIndex2, secondIndex2, cnt, cntSteps, flag = 0;
+        int cntEnd = 30;
+        private void buttonLevel2_Click(object sender, EventArgs e)// 2 УРОВЕНЬ
+        {
+            flag = 1;
+            resetFull();
+        }
+        private void buttonLevel1_Click(object sender, EventArgs e)// 1 УРОВЕНЬ
+        {
+            flag = 0;
+            resetFull();
+        }
+        private void buttonRestart_Click(object sender, EventArgs e) // НАЧАТЬ ЗАНОВО
+        {
+            resetFull();
+        }
+        public FormGame()
+        {
+            InitializeComponent();
+            Def = Bitmap.FromFile("images/standart.png");
+        }
+        private void FormGame_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(4);
+            ShowDefolt();
+            resetFull();
+        }
+        private async void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != Def)
+            {
+                MessageBox.Show("Выберите другую карту!");
+            }
+            else
+            {
+                if (firstIndex1 == -1 && secondIndex1 == -1)
+                {
+                    firstIndex1 = e.RowIndex;
+                    secondIndex1 = e.ColumnIndex;
+                    ShowItem(mass, firstIndex1, secondIndex1);
+                }
+                else
+                {
+                    firstIndex2 = e.RowIndex;
+                    secondIndex2 = e.ColumnIndex;
+                    ShowItem(mass, firstIndex2, secondIndex2);
+                    if (Math.Abs(mass[firstIndex1, secondIndex1]) != Math.Abs(mass[firstIndex2, secondIndex2]))
+                    {                      
+                        this.Enabled = false;
+                        await Task.Delay(700);
+                        this.Enabled = true;
+                        dataGridView1.Rows[firstIndex1].Cells[secondIndex1].Value = Def;
+                        dataGridView1.Rows[firstIndex2].Cells[secondIndex2].Value = Def;
+                    }
+                    else
+                    {
+                        cnt++;
+                    }
+                    firstIndex1 = -1;
+                    secondIndex1 = -1;
+                    cntSteps++;
+                    cntEnd--;
+                    textBoxCountSteps.Text = $"Кол-во сделанных шагов:{cntSteps}";
+                    textBoxPerfect.Text = $"Кол-во допустимых шагов: {cntEnd}";
+                    if (cnt == 8 || cntEnd == 0)
+                    {
+                        DialogResult result;
+                        if(cnt==8)
+                        {
+                             result = MessageBox.Show("Поздравляем c победой,хотите сыграть еще?", "ПОБЕДА!", MessageBoxButtons.YesNo);
+                        }
+                        else
+                        {
+                             result = MessageBox.Show("К сожалению вы проиграли,хотите попробовать заново?", "Проигрышь!", MessageBoxButtons.YesNo);
+                        }
+                        if (result == DialogResult.Yes)
+                        {
+                            resetFull();
+                        }
+                        else
+                        {
+                            //Application.Exit();
+                            this.Close();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
